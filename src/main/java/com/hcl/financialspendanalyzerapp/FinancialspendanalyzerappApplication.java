7fd99a8cdd0bd2d46f9976bd4f12de85a618c290
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-25 10:46:18 +0530

    initial commit.

---
+++ src/main/java/com/hcl/financialspendanalyzerapp/FinancialspendanalyzerappApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.financialspendanalyzerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialspendanalyzerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialspendanalyzerappApplication.class, args);
+	}
+
+}
