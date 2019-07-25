6a5af7e0a115a353fc1d1b0968e1285ebf3e9753
Rajat <bigdatarajatpancholi@gmail.com>
2019-07-25 14:04:11 +0530

    otp generation code

--- src/main/java/com/hcl/financialspendanalyzerapp/serviceimpl/OTPServiceImpl.java
+++ src/main/java/com/hcl/financialspendanalyzerapp/serviceimpl/OTPServiceImpl.java
@@ -39,12 +39,9 @@ public class OTPServiceImpl implements OTPService {
 	private String getRanOTP(int len) {
 		// Using numeric values
 		String numbers = "0123456789";
-
 		// Using random method
 		Random rndm_method = new Random();
-
 		char[] otp = new char[len];
-
 		for (int i = 0; i < len; i++) {
 			// Use of charAt() method : to get character value
 			// Use of nextInt() as it is scanning the value as int
