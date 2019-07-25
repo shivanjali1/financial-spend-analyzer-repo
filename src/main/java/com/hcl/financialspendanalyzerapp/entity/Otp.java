dd6798b2c9bf625a38d409aaeb98618e6da0e17e
sontrive <sontrive0310@gmail.com>
2019-07-25 12:41:19 +0530

    changes

--- src/main/java/com/hcl/financialspendanalyzerapp/entity/Otp.java
+++ src/main/java/com/hcl/financialspendanalyzerapp/entity/Otp.java
@@ -1,5 +1,40 @@
 package com.hcl.financialspendanalyzerapp.entity;
 
-public class Otp {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Data;
+
+@Data
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "opt_id")
+public class Otp implements Serializable {
+	
+	private static final long serialVersionUID = -8985679445647579333L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "opt_id")
+	private  Long optId;
+	
+	private String customerId;
+	
+	@Column(name = "time")
+	private LocalDateTime time;
+	
+	@Column(name = "oTP")
+	private String oTP;
+	
+	@Column(name = "transaction_id")
+	private Long transactionId;
 
 }
